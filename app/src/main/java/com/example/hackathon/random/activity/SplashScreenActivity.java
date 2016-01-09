855397bd8594daa59f229fbdc0cafc60148ad290
bach.vu <bach.vu@eastagile.com>
2016-01-10 00:18:27 +0700

    Move unlock screen to launch activity

--- app/src/main/java/com/example/hackathon/random/activity/SplashScreenActivity.java
+++ app/src/main/java/com/example/hackathon/random/activity/SplashScreenActivity.java
@@ -1,10 +1,14 @@
 package com.example.hackathon.random.activity;
 
+import android.app.KeyguardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
+import com.example.hackathon.random.BuildConfig;
 import com.example.hackathon.random.R;
 
 /**
@@ -17,6 +21,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        unlockCircleCiAvdScreen();
         setContentView(R.layout.activity_splash_screen);
 
         new Handler().postDelayed(new Runnable() {
@@ -27,5 +32,14 @@ public class SplashScreenActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIME_OUT);
+    }
+
+    private void unlockCircleCiAvdScreen() {
+        if (BuildConfig.DEBUG) {
+            KeyguardManager km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+            KeyguardManager.KeyguardLock keyguardLock = km.newKeyguardLock(MainActivity.class.getSimpleName());
+            keyguardLock.disableKeyguard();
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        }
     }
 }
